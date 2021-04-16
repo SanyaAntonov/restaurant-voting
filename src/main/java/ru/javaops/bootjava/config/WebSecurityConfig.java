b162a2dff82ab177cde2261ad6b6fd99730f5930
Alex Antonov <kritik145@gmail.com>
2021-04-16 05:41:13 +0300

    restaurant-voting v1.0

--- src/main/java/ru/javaops/bootjava/config/WebSecurityConfig.java
+++ src/main/java/ru/javaops/bootjava/config/WebSecurityConfig.java
@@ -59,8 +59,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/api/account/register").anonymous()
-                .antMatchers("/api/account").hasRole(Role.USER.name())
-                .antMatchers("/api/**").hasAnyRole(Role.ADMIN.name(), Role.USER.name())
+                .antMatchers("/api/account/**").hasRole(Role.USER.name())
+                .antMatchers("/api/**").hasRole(Role.ADMIN.name())
                 .and().httpBasic()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().csrf().disable();
