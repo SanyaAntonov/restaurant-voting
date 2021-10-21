80e160cc141cd843853833ed5ebad90e3273bb97
Alex Antonov <kritik145@gmail.com>
2021-10-21 12:57:25 +0300

    pom fix

--- src/main/java/ru/javaops/bootjava/repository/UserRepository.java
+++ src/main/java/ru/javaops/bootjava/repository/UserRepository.java
@@ -1,13 +1,11 @@
 package ru.javaops.bootjava.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
 import ru.javaops.bootjava.model.User;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    @RestResource(rel = "by-email", path = "by-email")
     Optional<User> findByEmailIgnoreCase(String email);
 }
