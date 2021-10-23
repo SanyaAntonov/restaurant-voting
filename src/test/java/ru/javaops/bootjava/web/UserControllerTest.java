65aa9d0970734b9856b69aff45426535379b8edc
Alex Antonov <kritik145@gmail.com>
2021-10-23 20:27:02 +0300

    UserControllerTest spring-data-rest

--- src/test/java/ru/javaops/bootjava/web/UserControllerTest.java
+++ src/test/java/ru/javaops/bootjava/web/UserControllerTest.java
@@ -15,64 +15,64 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class UserControllerTest extends AbstractControllerTest {
     private final String URL = "/api/users/";
 
-//    @Test
-//    @WithUserDetails(value = ADMIN_MAIL)
-//    void get() throws Exception {
-//        mockMvc.perform(MockMvcRequestBuilders.get(URL + USER_ID))
-//                .andExpect(status().isOk())
-//                .andDo(print());
-//    }
-//
-//    @Test
-//    @WithUserDetails(value = ADMIN_MAIL)
-//    void getAll() throws Exception {
-//        mockMvc.perform(MockMvcRequestBuilders.get(URL))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    @WithUserDetails(value = ADMIN_MAIL)
-//    void getByEmail() throws Exception {
-//        mockMvc.perform(MockMvcRequestBuilders.get(URL + "search/by-email?email=" + ADMIN_MAIL))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    @WithUserDetails(value = ADMIN_MAIL)
-//    void delete() throws Exception {
-//        mockMvc.perform(MockMvcRequestBuilders.delete(URL + USER_ID))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    @WithUserDetails(value = ADMIN_MAIL)
-//    void create() throws Exception {
-//        User user = new User();
-//        user.setEmail("new@gmail.com");
-//        user.setFirstName("New_First");
-//        user.setLastName("New_Last");
-//        user.setPassword("newpass");
-//        user.setRoles(List.of(Role.USER));
-//
-//        mockMvc.perform(MockMvcRequestBuilders.post(URL)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsBytes(user)))
-//                .andExpect(status().isOk());
-//    }
-//
-//    @Test
-//    @WithUserDetails(value = ADMIN_MAIL)
-//    void update() throws Exception {
-//        User user = new User();
-//        user.setEmail("new@gmail.com");
-//        user.setFirstName("New_First");
-//        user.setLastName("New_Last");
-//        user.setPassword("newpass");
-//        user.setRoles(List.of(Role.USER));
-//
-//        mockMvc.perform(MockMvcRequestBuilders.put(URL + USER_ID)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsBytes(user)))
-//                .andExpect(status().isOk());
-//    }
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void get() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(URL + USER_ID))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getAll() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(URL))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void getByEmail() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(URL + "search/by-email?email=" + ADMIN_MAIL))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void delete() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete(URL + USER_ID))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void create() throws Exception {
+        User user = new User();
+        user.setEmail("new@gmail.com");
+        user.setFirstName("New_First");
+        user.setLastName("New_Last");
+        user.setPassword("newpass");
+        user.setRoles(List.of(Role.USER));
+
+        mockMvc.perform(MockMvcRequestBuilders.post(URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(user)))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    @WithUserDetails(value = ADMIN_MAIL)
+    void update() throws Exception {
+        User user = new User();
+        user.setEmail("new@gmail.com");
+        user.setFirstName("New_First");
+        user.setLastName("New_Last");
+        user.setPassword("newpass");
+        user.setRoles(List.of(Role.USER));
+
+        mockMvc.perform(MockMvcRequestBuilders.put(URL + USER_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(user)))
+                .andExpect(status().isNoContent());
+    }
 }
